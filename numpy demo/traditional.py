8e619a0dd26c765d3b1f1dbfdf2b26a4e6a48af0
OMKAR JADHAV <jadhavos43@gmail.com>
2017-11-02 00:06:14 +0530

    Cleaning of project : Done

--- numpy demo/traditional.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-
-if __name__ == '__main__':
-    with open('winequality-red.csv', 'r') as f:
-        wines = list(csv.reader(f, delimiter=';'))
-
-    qualities = [float(item[-1]) for item in wines[1:]]
-    print sum(qualities) / len(qualities)
